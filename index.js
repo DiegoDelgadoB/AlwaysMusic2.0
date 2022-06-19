4ceff65fc6a5d591c12b3ea6aaf9d0b537b0ac85
Diego Delgado Barra <diego.delgadobarra@outlook.com>
2022-06-19 14:59:31 -0400

    SecondCommit

--- index.js
+++ index.js
@@ -47,4 +47,57 @@ yargs.command("registrar", "Insertar registro de estudiante", {
     } catch (error) {
         console.log(mensajesErrores(error.code));
     }
+}).command("consultar","Obtener lista de todos los estudiantes y estudiante por rut", {
+    rut: {
+        describe: 'Identificación única del estudiante',
+        demand: false,
+        alias: 'r'
+    }
+}, async (argumentos) => {
+    try {
+        if(argumentos.rut){
+            const config = {
+                text: "SELECT * FROM estudiantes WHERE rut=$1", // Consultar estudiante por rut.
+                values: [argumentos.rut]
+            };
+            let res = await pool.query(config);
+            if (res.rows.length == 0){
+                console.log("Rut incorrecto, por favor verifique la lista");
+            }else{
+                console.table(res.rows);
+            }
+        }else{
+            let sql = "SELECT * FROM estudiantes"; // Consultar los estudiantes registrados.
+            let res = await pool.query(sql);
+            console.table(res.rows);
+        }
+    } catch (error) {
+        console.log(mensajesErrores(error.code));
+    }
+}).command("editar", "Actualizar registro de estudiante", {
+    rut: {
+        describe: 'Identificación única del estudiante',
+        demand: true,
+        alias: 'r'
+    },
+    nombre: {
+        describe: 'Nombre del estudiante',
+        demand: true,
+        alias: 'n'
+    },
+    curso: {
+        describe: 'Curso al que se inscribe el estudiante',
+        demand: true,
+        alias: 'c'
+    },
+    nivel: {
+        describe: 'Nivel del estudiante',
+        demand: true,
+        alias: 'nv'
+    }
+}, async(argumentos) => {
+    let { rut, nombre, curso, nivel } = argumentos;
+    const config = {
+        text: "UPDATE estudiantes SET nombre=$1, curso=$2, nivel=$3 WHERE rut=$4 RETURNING *", // Actualizar la información de un estudiante.
+    } 
 })
